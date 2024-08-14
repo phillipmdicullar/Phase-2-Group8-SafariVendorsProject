09cf9ec4419a95073056d1a3f1fd9976da845b57
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-08-14 22:34:07 +0300

    created the product card

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import ProductCard from "./ProductCard";
 
 export default function App() {
   
   return (
     
-    <div className="text-3xl font-bold underline">
+    <div>
       <ProductCard />
     </div>
   
