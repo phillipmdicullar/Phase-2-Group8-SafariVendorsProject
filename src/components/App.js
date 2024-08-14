a32c11109912b1cb752d9eb99e29dea1f279412c
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-08-14 15:19:03 +0300

    Fetched data from db.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import ProductCard from "./ProductCard";
+
 export default function App() {
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      <h1>Everything works fine</h1>
-    </h1>
-  )
+    
+    <div className="text-3xl font-bold underline">
+      <ProductCard />
+    </div>
+  
+)
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// import React from 'react'
+
+
 
 function ProductCard() {
-  return (
-    <div>ProductCard</div>
-  )
+    const [products, setProducts] =useState([])
+
+useEffect(() => {
+  fetch('http://localhost:3000/products')
+      .then(res =>res.json())
+      .then(data =>setProducts(data))
+      .catch(err=>console.log(err))
+}, ([]))
+
+const displayProducts = products.map(product => {
+    return (
+       <div className=' grid-cols-4 grid '>
+        <div key={product.id} className=''>
+              <img src={product.image} alt={product.name} />
+              <h3>{product.name}</h3>
+              <p>{product.category}</p>
+              <p>{product.vendor}</p>
+          </div>
+        </div>
+    )
+})
+
+return(
+    <div>
+        {displayProducts}
+    </div>
+)
 }
 
 export default ProductCard
